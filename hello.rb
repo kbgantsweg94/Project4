e640b4f2adee03db4dc947f7c8c5fc0a984ea497
Kevin Gantsweg <kgantsweg@comcast.net>
2014-10-30 15:06:34 -0700

    Using ARGV

--- hello.rb
+++ hello.rb
@@ -1,1 +1,14 @@
-"Hello, World"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2)
+    {
+      std::cerr << "Usage: " << argv[0] << " NAME" << std::endl;
+      return 1;
+    }
+    std::cout << "Hello, " << argv[1] << "!" << std::endl;
+    return 0;
+}
